1e26f1f80438ea8ad84496919df4778cc883135b
rawfid <rawfid.shuvo@outlook.com>
2025-11-30 23:42:56 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/conspiracy-card-game/', // MUST match your repo name
+  base: '/pirates-game/', // MUST match your repo name
 })
